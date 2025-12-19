ac1d19d52a6c16a0a8fa055ed4c281e42158d95f
Harsh Kumar <139849981+harsh2005-bit@users.noreply.github.com>
2025-12-19 15:34:47 +0530

    Update contact details and footer copyright
    Updated contact information and footer copyright notice.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -32,9 +32,9 @@ const Footer = () => {
         {
             title: "CONTACT",
             links: [
-                { text: "+1-212-456-7890", path: '/', icon: MailIcon },
-                { text: "contact@example.com", path: '/', icon: PhoneIcon },
-                { text: "794 Francisco, 94102", path: '/', icon: MapPinIcon }
+                { text: "+91 7667161841", path: '/', icon: MailIcon },
+                { text: "harshjhabksc@gmail.com", path: '/', icon: PhoneIcon },
+                { text: "Jalandhar Punjab", path: '/', icon: MapPinIcon }
             ]
         }
     ];
@@ -80,7 +80,7 @@ const Footer = () => {
                     </div>
                 </div>
                 <p className="py-4 text-sm text-slate-500">
-                    Copyright 2025 © gocart All Right Reserved.
+                    Copyright 2025 © gocart All Right Reserved. Made with ❤️ by Harsh.
                 </p>
             </div>
         </footer>
